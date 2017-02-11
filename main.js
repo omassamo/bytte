3dc68e4bcce6f178fea5369f00b4d6b0d955e27b
omassamo <samodenmark@gmail.com>
2017-02-11 13:56:45 +0100

    API key variable

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function listItems() {
-axios.get("https://api.airtable.com/v0/appA2Ta4a0stQkxMY/items?maxRecords=3&view=Main%20View&api_key=keycGQqA0T3nBwQay")
+// read-only api key
+api_key = "keyALw6H4JrUhpfbl"
+
+// get list of records
+function listRecords() {
+axios.get("https://api.airtable.com/v0/appA2Ta4a0stQkxMY/items?maxRecords=3&view=Main%20View&api_key="+api_key)
 	.then(function (response) {
 		console.log(response);
 	})
@@ -7,4 +11,16 @@ axios.get("https://api.airtable.com/v0/appA2Ta4a0stQkxMY/items?maxRecords=3&view
     	console.log(error);
   });
 };
+
+// get specific record
+function retrieveRecord() {
+axios.get("https://api.airtable.com/v0/appA2Ta4a0stQkxMY/items/recFtcsmEsrFen18y&api_key="+api_key)
+	.then(function (result) {
+		console.log(result);
+	})
+	.catch(function (error) {
+    	console.log(error);
+  });
+};
+
 
